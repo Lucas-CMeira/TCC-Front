9283d43d954c150e6ee84eabef1b8605403043e8
Lucas-CMeira <lucascmeira2005@gmail.com>
2026-02-19 20:51:21 -0300

    ProfillePageATT

--- src/pages/profile/profile-page.tsx
+++ src/pages/profile/profile-page.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { CgProfile } from "react-icons/cg";
 
 const ProfilePage = () => {
   return (
-    <div>
-      ProfilePage
+    <div className='min-h-screen bg-slate-100 p-6 m-8'>
+      <div className='flex flex-col justify-center items-center'>
+          <CgProfile  className='w-auto h-56 m-7'/>
+      </div>
     </div>
   )
 }
